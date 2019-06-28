b91ca47c376d0f21416bb08dae66e4328376ca49
Andrey Samoilov <heyhahch@outlook.com>
2019-06-28 19:37:20 +0100

    Add helper command for research

--- FactorioBot/cogs/factoriohelper.py
+++ FactorioBot/cogs/factoriohelper.py
@@ -36,7 +36,7 @@ class FactorioHelper(commands.Cog):
     async def crafting_help(self, ctx):
         embed = discord.Embed(title="Crafting Help", description="These are all the possible items to craft.\n More "
                                                                  "info at https://stable.wiki.factorio.com/Data.raw. "
-                                                                 "Anything under recpie should work. ", color=0x00ff00)
+                                                                 "Anything under recipe should work. ", color=0x00ff00)
         dirname = os.path.dirname(__file__)
         filename = os.path.join(dirname, '../items.txt')
         with open(filename, "r") as fp:
@@ -56,6 +56,29 @@ class FactorioHelper(commands.Cog):
 
         await ctx.author.send(embed=embed)
 
+    @commands.command()
+    async def research_help(self, ctx):
+        embed = discord.Embed(title="Research Help", description="These are all the possible techs to research.\n More "
+                                                                 "info at https://stable.wiki.factorio.com/Data.raw. "
+                                                                 "Anything under `technology` should work. ", color=0x00ff00)
+        dirname = os.path.dirname(__file__)
+        filename = os.path.join(dirname, '../techs.txt')
+        with open(filename, "r") as fp:
+            buffer = 0
+            stringBuilder = ""
+            count = 1
+            for line in fp:
+                if (buffer + len(line)) > 1024:
+                    embed.add_field(name="Techs part " + str(count), value=stringBuilder, inline=False)
+                    stringBuilder = line
+                    buffer = len(stringBuilder)
+                    count += 1
+                else:
+                    buffer += len(line)
+                    stringBuilder += line
+            embed.add_field(name="Techs part " + str(count), value=stringBuilder, inline=False)
+
+        await ctx.author.send(embed=embed)
 
 # Setups cog
 def setup(bot):
